8adedbfad983ba57b4f2420ae7b0c0f381b4cd12
AngadSC <104170661+AngadSC@users.noreply.github.com>
2025-12-27 14:48:50 -0700

    addde gitingore for env vriables

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,11 @@
- 
+from pydantic_settings import BaseSettings
+from typing import List
+import os
+
+class Setting(BaseSettings): 
+    DATABASE_URL: 
+    REDIS_URL: 
+
+    #JWT
+    SECRET_KEY : 
+    ALGORITHM: 
